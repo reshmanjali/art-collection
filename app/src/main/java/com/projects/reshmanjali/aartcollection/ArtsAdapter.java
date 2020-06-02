5a792a65f1987dcc6be5b1ee060c877ffdd0a989
Reshmanjali Guntupalli <reshmanjali.g@gmail.com>
2020-06-02 17:39:28 +0530

    added pics, loader

--- app/src/main/java/com/projects/reshmanjali/aartcollection/ArtsAdapter.java
+++ app/src/main/java/com/projects/reshmanjali/aartcollection/ArtsAdapter.java
@@ -1,14 +1,14 @@
 package com.projects.reshmanjali.aartcollection;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
+import com.squareup.picasso.Picasso;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,9 +17,11 @@ class ArtsAdapter extends RecyclerView.Adapter<ArtsAdapter.MyViewHolder> {
 
     ListPojo validObjectsListPojo;
     int[] objectIds;
+    Context context;
     MetMuseumService metMuseumService;//= RetrofitInstance.getRetrofitInstance().create(MetMuseumService.class);
 
-    public ArtsAdapter(ListPojo responseListPojo) {
+    public ArtsAdapter(ListPojo responseListPojo, Context context) {
+        this.context = context;
         validObjectsListPojo = responseListPojo;
         objectIds = validObjectsListPojo.getObjectIds();
         metMuseumService = RetrofitInstance.getRetrofitInstance().create(MetMuseumService.class);
@@ -44,7 +46,14 @@ class ArtsAdapter extends RecyclerView.Adapter<ArtsAdapter.MyViewHolder> {
         arObject.enqueue(new Callback<ArtObjectPojo>() {
             @Override
             public void onResponse(Call<ArtObjectPojo> call, Response<ArtObjectPojo> response) {
-                myViewHolder.artName.setText(response.body().getTitle());
+                ArtObjectPojo body = response.body();
+                myViewHolder.artName.setText(body.getTitle());
+                myViewHolder.artCulture.setText("Culture: "+ body.getCulture());
+                myViewHolder.artCreditLine.append("Credit Line "+ body.getCreditLine());
+                if(body.getPrimaryImage() != "")
+                    Picasso.with(context).load(body.getPrimaryImage()).into(myViewHolder.artImg);
+                else
+                    Picasso.with(context).load(R.drawable.no_img_placeholder).into(myViewHolder.artImg);
             }
 
             @Override
@@ -52,20 +61,24 @@ class ArtsAdapter extends RecyclerView.Adapter<ArtsAdapter.MyViewHolder> {
                 myViewHolder.artName.setText("Uff Sorry");
             }
         });
-       // myViewHolder.artName.setText("hgyghb");
-
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 20;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView artName;
+        ImageView artImg;
+        TextView artCulture;
+        TextView artCreditLine;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             artName = itemView.findViewById(R.id.id_art_name_tv);
+            artImg = itemView.findViewById(R.id.id_art_img_iv);
+            artCulture = itemView.findViewById(R.id.id_art_culture_tv);
+            artCreditLine = itemView.findViewById(R.id.id_art_cLine_tv);
         }
     }
 }
--- app/src/main/java/com/projects/reshmanjali/aartcollection/MainActivity.java
+++ app/src/main/java/com/projects/reshmanjali/aartcollection/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import retrofit2.Call;
@@ -11,14 +13,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     RecyclerView recyclerView;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.id_tv);
         recyclerView = findViewById(R.id.id_rv);
+        progressBar = findViewById(R.id.id_pro_bar);
         final ListPojo responseListPojo = new ListPojo();
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
@@ -27,13 +29,14 @@ public class MainActivity extends AppCompatActivity {
         validObjectListCall.enqueue(new Callback<ListPojo>() {
             @Override
             public void onResponse(Call<ListPojo> call, Response<ListPojo> response) {
+                progressBar.setVisibility(View.GONE);
                 responseListPojo.setObjectIds(response.body().getObjectIds());
                 responseListPojo.setTotal(response.body().total);
-                recyclerView.setAdapter(new ArtsAdapter(responseListPojo));
+                recyclerView.setAdapter(new ArtsAdapter(responseListPojo,getApplicationContext()));
             }
             @Override
             public void onFailure(Call<ListPojo> call, Throwable t) {
-                textView.setText("Sorry");
+               // textView.setText("Sorry");
             }
         });
 
